4a1360d57e0a4626b581373885ba691be9228d63
Page Huang <page.huang@firstleap.cn>
2016-10-25 20:37:59 +0800

    sc

--- index.ios.js
+++ index.ios.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 
 let App = require('./js/list3')
+let App2 = require('./js/scrollView.js')
 
 export default class ExpandAnimated extends Component {
   render() {
     return (
-      <App />
+      <App2 />
     );
   }
 }
--- js/list3.js
+++ js/list3.js
@@ -9,7 +9,8 @@ import {
 	LayoutAnimation,
 	Modal,
 	Dimensions,
-	PanResponder
+	PanResponder,
+	ScrollView,
 } from 'react-native';
 
 class Button extends React.Component {
@@ -42,8 +43,11 @@ class Button extends React.Component {
   }
 }
 
+var dataList = ['row 1', 'row 2','row 3','row 4','row 5'];
 var myMap = new Map();
 const NUM = 30;
+const defaultWidth = 214;
+const defaultHeight = 264;
 
 class list3 extends React.Component {
 
@@ -52,13 +56,11 @@ class list3 extends React.Component {
 		let num = 30;
 		const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 		this.state = {
-			dataSource: ds.cloneWithRows(['row 1', 'row 2','row 3','row 4', 'row 5','row 6']),
-			expandingFlag: 0,
-			isOpen: false,
-			backFrame: {t: 0, w: 214, h: 264},
-			frontFrame: {t: 0, w: 214, h: 264, mr: 0},
-			backMarginLeft: 20,
+			dataSource: ds.cloneWithRows(dataList),
 			isScrollEnabled: true,
+			stateDict: myMap,
+			currentIndex: 0,
+			theWidth: 100
 		}
 
 		this._panResponder = PanResponder.create({
@@ -98,62 +100,69 @@ class list3 extends React.Component {
   }
 
 	_pressHandle(rowID: number) {
-		console.log('_pressHanfasfasfasdle==' + rowID);
-
+		console.log("_pressHandle=>" + this.state.stateDict.get(rowID));
 		LayoutAnimation.linear();
-		let num = 30;
-		if (this.state.isOpen === false) {
+		if ((this.state.stateDict.get(rowID) === 0) || !this.state.stateDict.get(rowID)) {
+			console.log("_pressHandle++0");
 			myMap.set(rowID, 1)
-			console.log('_pressHandle==false++'+ myMap.get(rowID));
 			this.setState ({
-				expandingFlag: 1,
-				isScrollEnabled: true
+				isScrollEnabled: true,
+				stateDict: myMap,
+			})
+		} else if (this.state.stateDict.get(rowID) === 1) {
+			console.log("_pressHandle--1");
+			myMap.set(rowID, 2)
+			this.setState ({
+				isScrollEnabled: true,
+				stateDict: myMap,
 			})
 		} else {
-			myMap.set(rowID, 2)
-			console.log('_pressHandle==true++'+ myMap.get(rowID));
+			console.log("_pressHandle***3");
+			myMap.set(rowID, 0)
 			this.setState ({
-				expandingFlag: 2,
 				isScrollEnabled: true,
+				stateDict: myMap,
 			})
 		}
 	}
 
 	_close(rowID) {
+		console.log("_close==" + rowID + "state=" + this.state);
 		LayoutAnimation.linear();
-		myMap.set(rowID, 0)
-		let num = 0;
-		if (this.state.frontFrame.t === -120) {
-			num = 30;
+		if (this.state.stateDict.get(rowID) === 2) {
+			myMap.set(rowID, 1)
+			this.setState ({
+				isScrollEnabled: true,
+				stateDict: myMap
+			})
+		} else {
+			myMap.set(rowID, 0)
+			this.setState ({
+				isScrollEnabled: true,
+				stateDict: myMap
+			})
 		}
-
-		this.setState ({
-			expandingFlag: 0,
-			isOpen: num > 0? true : false,
-			isScrollEnabled: true
-		})
 	}
 
 	getBackViewStyle(rowID) {
-		let expandingFlag = myMap.get(rowID);
-		if (this.state.expandingFlag === expandingFlag) {
+		if (this.state.stateDict.get(rowID) === 1) {
 			return [
 				styles.back,
 				{
 					top: 0,
-					width: 214 + NUM,
-					height: 264 + NUM,
+					width: defaultWidth + NUM ,
+					height: defaultHeight + NUM,
 					marginLeft: 0,
 				}
 			]
-		} else if (this.state.expandingFlag === expandingFlag) {
+		} else if (this.state.stateDict.get(rowID) === 2 ) {
 			return [
 				styles.back,
 				{
 					top: 120,
 					width: Dimensions.get('window').width,
-					height: Dimensions.get('window').height - 120,
-					marginLeft: 20
+					height: Dimensions.get('window').height,
+					marginLeft: 0
 				}
 			]
 		} else {
@@ -161,8 +170,8 @@ class list3 extends React.Component {
 				styles.back,
 				{
 					top: 0,
-					width: 214,
-					height: 264,
+					width: defaultWidth,
+					height: defaultHeight,
 					marginLeft: 20
 				}
 			]
@@ -171,18 +180,22 @@ class list3 extends React.Component {
 
 	getFrontViewStyle(rowID) {
 
-		let expandingFlag = myMap.get(rowID);
-		console.log("getFrontViewStyle+++" + rowID);
-		if (this.state.expandingFlag === expandingFlag) {
+		// for (var [key, value] of myMap) {
+		//   console.log(key + " = " + value);
+		// }
+		if (this.state.stateDict.get(rowID) === 1) {
+			// 1.展开
 			return [
 				styles.front,
 				{
 					top: -NUM,
-					width: 214,
-					height: 264,
+					width: defaultWidth,
+					height: defaultHeight,
+					marginLeft: NUM/2
 				}
 			]
-		} else if (this.state.expandingFlag === expandingFlag) {
+		} else if (this.state.stateDict.get(rowID) === 2) {
+			// 2.全展开
 			return [
 				styles.front,
 				{
@@ -192,21 +205,55 @@ class list3 extends React.Component {
 				}
 			]
 		} else {
+			// 0.关闭
 			return [
 				styles.front,
 				{
 					top: 0,
-					width: 214,
-					height: 264,
+					width: defaultWidth,
+					height: defaultHeight,
+				}
+			]
+		}
+	}
+
+	getMainListViewStyle(rowID) {
+		console.log("currentIndex=" + this.state.currentIndex);
+		if (this.state.currentIndex === 0) {
+			return [
+				styles.container,
+				{
+					width: Dimensions.get('window').width - 100,
+				}
+			]
+		} else if (this.state.currentIndex === rowID ) {
+			return [
+				styles.container,
+				{
+					width: Dimensions.get('window').width -200,
+					marginLeft: 100
+				}
+			]
+		} else if (this.state.currentIndex + 1 === rowID ) {
+			return [
+				styles.container,
+				{
+					width: Dimensions.get('window').width,
+				}
+			]
+		} else if (this.state.currentIndex -1 === rowID ) {
+			return [
+				styles.container,
+				{
+					width: Dimensions.get('window').width,
 				}
 			]
 		}
 	}
 
 	_renderRow(rowData: string, sectionID: number, rowID: number) {
-		console.log("_renderRow=" + rowID);
 		return (
-			<View style={[styles.container, { width: Dimensions.get('window').width}]}>
+			<View style={styles.container} >
 					<View style={this.getBackViewStyle(rowID)}>
 						<TouchableHighlight onPress={ this._pressHandle.bind(this, rowID)} >
 							<View style={this.getFrontViewStyle(rowID)}
@@ -223,15 +270,42 @@ class list3 extends React.Component {
 		)
 	}
 
+	handleScroll() {
+		return (
+			<View style={[styles.container, {marginLeft: 0 * Dimensions.get('window').width}]} >
+					<View style={this.getBackViewStyle(0)}>
+						<TouchableHighlight onPress={ this._pressHandle.bind(this, 0)} >
+							<View style={this.getFrontViewStyle(0)}
+								// {...this._panResponder.panHandlers}
+							>
+							<Text>324</Text>
+								<Button onPress={ this._close.bind(this, 0)} style={styles.closeButton}>
+									Close
+								</Button>
+							</View>
+						</TouchableHighlight>
+					</View>
+			</View>
+		)
+	}
 	render() {
 		return (
-			<ListView
-				horizontal={true}
-				pagingEnabled={true}
-				scrollEnabled={this.state.isScrollEnabled}
-				dataSource={this.state.dataSource}
-				renderRow={this._renderRow.bind(this)}
-			/>
+			// <ListView
+			// 	horizontal={true}
+			// 	pagingEnabled={true}
+			// 	scrollEnabled={this.state.isScrollEnabled}
+			// 	dataSource={this.state.dataSource}
+			// 	renderRow={this._renderRow.bind(this)}
+			// />
+
+			<ScrollView
+          horizontal={true}
+          style={[styles.scrollView, styles.horizontalScrollView]}>
+				<View style={{width: 2000}}>
+					{this.handleScroll()}
+				</View>
+      </ScrollView>
+
 		)
 	}
 }
@@ -241,7 +315,8 @@ const styles = StyleSheet.create({
 		flex: 1,
 		alignItems: 'center',
 		justifyContent: 'center',
-		backgroundColor: 'gray'
+		backgroundColor: 'transparent',
+		width: Dimensions.get('window').width,
 	},
 	front: {
 		backgroundColor: 'red',
@@ -255,6 +330,9 @@ const styles = StyleSheet.create({
 		top: 30,
 		marginRight: 15,
 		alignItems: 'flex-end',
+	},
+	scrollView: {
+		width: 20000//dataList.length * Dimensions.get('window').width
 	}
 
 });
